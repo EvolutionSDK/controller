04d094761c9a3b0013182e76d0cae7f895f240d2
Nate Ferrero <nateferrero@gmail.com>
2012-04-04 17:32:59 -0600

    Controller

--- library/api-controller.php
+++ library/api-controller.php
@@ -392,14 +392,16 @@ abstract class ApiList extends ApiController implements Iterator, Countable {
 			throw new Exception('You need to set `$this->method` in' . get_class($this));
 
 		$this->bundle = strtolower($this->bundle);
-		$this->method = strtolower($this->method);
+		//$this->method = strtolower($this->method);
 
 		/**
 		 * Set the List
 		 */
+		if(!isset(e::${$this->bundle}))
+			throw new Exception("Bundle `$this->bundle` is not installed");
 		$this->list = e::${$this->bundle}->{'get'.$this->method}($id);
 		if(!($this->list instanceof \Bundles\SQL\ListObj))
-			throw new Exception('`$this->bundle` and `$this->method` must requrn a instance of `Bundles\SQL\ListObj` in `' . get_class($this) . '`');
+			throw new Exception("`e::$$this->bundle->get$this->method()` must return a instance of `Bundles\SQL\ListObj` in `" . get_class($this) . '`');
 
 		$this->_filterList(false, $this->searchFields);
 		
