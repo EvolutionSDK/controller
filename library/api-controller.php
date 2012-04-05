5cda4fbd5fdd060cb1a8f4a02e1c62d2768d6bd9
Nate Ferrero <nateferrero@gmail.com>
2012-04-05 17:42:46 -0600

    Debug

--- library/api-controller.php
+++ library/api-controller.php
@@ -439,7 +439,7 @@ abstract class ApiList extends ApiController implements Iterator, Countable {
 	public function filter($var, $val) {
 		$this->list->condition($var, $val);
 		if(isset($_GET['--api-list-filter']))
-			dump($this->list);
+			eval(d);
 		return $this;
 	}
 
